08a3830f1dcd9718bef28967eab9a7d0d090b018
MaksMoroz11 <maksim.morozov2706@gmail.com>
2026-04-16 18:05:51 +0300

    dev: added more sections in landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
 import Features from './components/Features/Features'
+import Workflow from './components/Workflow/Workflow'
 import Preview from './components/Preview/Preview'
+import TechSpecs from './components/TechSpecs/TechSpecs'
+import FAQ from './components/FAQ/FAQ'
 import CTA from './components/CTA/CTA'
 import Footer from './components/Footer/Footer'
 
@@ -13,7 +16,10 @@ function App() {
 			<main>
 				<Hero />
 				<Features />
+				<Workflow />
 				<Preview />
+				<TechSpecs />
+				<FAQ />
 				<CTA />
 			</main>
 			<Footer />
