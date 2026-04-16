f299f75eeda80dee684a3313f1824da2eb324768
MaksMoroz11 <maksim.morozov2706@gmail.com>
2026-04-16 17:29:30 +0300

    dev: updated landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,23 @@
-import './App.css'
+import React from 'react'
 import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
+import Features from './components/Features/Features'
+import Preview from './components/Preview/Preview'
+import CTA from './components/CTA/CTA'
+import Footer from './components/Footer/Footer'
 
-export default function App() {
+function App() {
 	return (
-		<div className="container">
+		<div className="app">
 			<Header />
 			<main>
 				<Hero />
+				<Features />
+				<Preview />
+				<CTA />
 			</main>
+			<Footer />
 		</div>
 	)
 }
+export default App
